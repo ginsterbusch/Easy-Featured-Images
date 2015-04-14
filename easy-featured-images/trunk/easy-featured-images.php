ab20599d208de2c7d703acf46d24bbbd49d174d3
Daniel Pataki <daniel@bonsaished.com>
2015-04-14 07:36:39 +0200

    Incremented version

--- easy-featured-images/trunk/easy-featured-images.php
+++ easy-featured-images/trunk/easy-featured-images.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:       Easy Featured Images
 Description:       Adds featured images to the admin post lists and allows you to add and modify them without loading the post's edit page.
-Version:           1.0.0
+Version:           1.1.0
 Author:            Daniel Pataki
 Author URI:        http://danielpataki.com/
 License:           GPLv2 or later
